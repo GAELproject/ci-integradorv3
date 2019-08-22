e00a111f304ac91f54510108684973a94e090329
Diogo da Silva Lima <diogo.s@academico.ifrn.edu.br>
2019-08-22 14:22:21 -0300

    update database-config. uptade tela-login. uptade style-css.

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -2,13 +2,14 @@
 <html lang="pt-br">
 <head>
   <meta charset="utf-8">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
   <meta name="author" content="GeeksLabs">
   <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
   <link rel="shortcut icon" href="<?php echo base_url('assets/img/gael.ico')?>">
 
-  <title>Sistema Integrado de Gerenciamento de Eletroeletrônicos - Login</title>
+  <title>GAEL - Login</title>
 
   <!-- Bootstrap CSS -->
   <link href="<?php echo base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
@@ -22,44 +23,37 @@
   <link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
   <link href="<?php echo base_url('assets/css/style-responsive.css')?>" rel="stylesheet" />
 
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
-  <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- =======================================================
-      Theme Name: NiceAdmin
-      Theme URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-      Author: BootstrapMade
-      Author URL: https://bootstrapmade.com
-    ======================================================= -->
+ 
 </head>
 
-<body class="login-img3-body">
+<body class="">
 
   <div class="container">
 
-    <form class="login-form" action="<?php echo base_url('index.php/gael/home')?>">
-      <div class="login-wrap">
+
+
+    
+    <form class="login-form form-login-container" action="<?php echo base_url('index.php/gael/home')?>">
+      <div id="" class="login-wrap ">
         <p class="login-img"><img src="<?php echo base_url('assets/img/gael.png')?>" width="80"></i></p>
         <h4 class="text-center" style="color: black;">
-          Sistema Integrado de Gerenciamento de Eletroeletrônicos
+          <strong>Sistema Integrado de Gerenciamento de Eletroeletrônicos</strong>  
+        
         </h4 >
         <div class="input-group">
-          <span class="input-group-addon"><i class="icon_profile"></i></span>
-          <input type="text" class="form-control" placeholder="Login" autofocus>
+          <span class="input-group-addon blr"><i class="icon_profile"></i></span>
+          <input type="text" class="form-control brr" placeholder="Login" autofocus>
         </div>
         <div class="input-group">
-          <span class="input-group-addon"><i class="icon_key_alt"></i></span>
-          <input type="password" class="form-control" placeholder="Senha">
+          <span class="input-group-addon blr"><i class="icon_key_alt"></i></span>
+          <input type="password" class="form-control brr" placeholder="Senha">
         </div>
         <label style="color: black;" class="checkbox">
                 <input  type="checkbox" value="remember-me">
                 Lembrar
                 <span  class="pull-right"> <a href="" style="color: black;">Esqueceu a senha?</a></span>
             </label>
-          <button type="submit" class="text-light btn btn-primary btn-lg btn-block" >Logar </button>
+          <button type="submit" class="text-light btn btn-success btn-lg btn-block br-5" >Logar </button>
 
       </div>
     </form>
