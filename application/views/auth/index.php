cbaa9b8cef3501713118f3401053986c68181e23
Diogo da Silva Lima <diogo.s@academico.ifrn.edu.br>
2019-08-22 14:31:45 -0300

    update tema include meta tag

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
   <meta charset="utf-8">
-
+  <meta name="theme-color" content="#4cd964">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
   <meta name="author" content="GeeksLabs">
