aa1bb8a0a15c8b959377a013ed3787fa7d17e8fc
Diogo da Silva Lima <diogo.s@academico.ifrn.edu.br>
2019-09-23 14:04:20 -0300

    início do crud-relizar atividade em equipamentos e ajustes no front do projeto

---
+++ application/models/Equipamento_realizou_atividade_model.php
@@ -0,0 +1,57 @@
+<?php
+class Equipamento_model extends CI_Model
+{
+	public $id_equipamento_realizou_atividade;
+	public $equipamento_id_equipamento;
+	public $atividade_id_atividade;
+	public $data_hora_atividade;
+
+    
+	public function __construct(){
+		 parent::__construct();
+	}
+
+
+	public function inserir()
+	{
+        $dados = array("id_equipamento_realizou_atividade" => $this->id_equipamento_realizou_atividade,
+                        "equipamento_id_equipamento" => $this->equipamento_id_equipamento,
+						"atividade_id_atividade" => $this->atividade_id_atividade,
+						"data_hora_atividade" => $this->data_hora_atividade);
+
+		return $this->db->insert('equipamento',$dados);
+	}
+	public function recuperar(){
+		$query = $this->db->get('equipamento');
+		return $query->result();
+	}
+
+   
+
+	public function recuperarUm($id){
+        $this->db->where('id_meta',$id);
+        $query = $this->db->get('meta');
+        return $query->row();
+    }
+	public function update(){
+        $this->db->set('titulo', $this->titulo);
+        $this->db->set('descricao', $this->descricao);
+		$this->db->set('turno', $this->turno);
+		$this->db->set('data_criacao', $this->data_criacao);
+        $this->db->set('data_prazo_finalizacao', $this->data_prazo_finalizacao);
+        $this->db->set('data_finalizacao', $this->data_finalizacao);
+        $this->db->set('situacao', $this->situacao);
+        $this->db->set('id_criador',$this->id_criador);
+
+        $this->db->where('id_meta', $this->id_meta);
+        $this->db->update('meta');
+
+	}
+	//função para deletar uma meta.
+	public function delete($id)
+    {
+        $this->db->where('id_meta', $id);
+        $this->db->delete('meta');
+    }
+
+}
