66de7855b1e7e3e12e74f528b0a35f7c55b340cd
juliana <julianabeatriz890@gmail.com>
2019-11-09 01:34:09 -0200

    Ajustes na tela inicial

--- application/views/n-autenticado/elixo.php
+++ application/views/n-autenticado/elixo.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Landing Page - Start Bootstrap Theme</title>
+  <title>E-lixo</title>
 
   <!-- Bootstrap core CSS -->
   
@@ -21,7 +21,9 @@
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
   <!-- Custom styles for this template -->
-  <link href="<?= base_url('assets/css/landing-page.min.css') ?> " rel="stylesheet">
+  <link href="<?php echo base_url('assets/css/landing-page.min.css') ?> " rel="stylesheet"/>
+  
+  <link href="<?php echo base_url('css/landing-page.min.css')?>" rel="stylesheet">
 
 </head>
 
@@ -36,13 +38,13 @@
   </nav>
 
   <!-- Masthead -->
-  <header class="masthead text-white text-center">
+  <header class="masthead text-white text-center" style="background-image: url(<?= base_url('assets/images/bg-masthead.png')?>);">
     <div class="overlay"> </div>
     <div class="container">
       <div class="row">
            <div class="col-xl-9 mx-auto">
            
-          <h1 class="mb-5">Saiba mais sobre o E-lixo</h1>
+          <h1 class="mb-5">E-lixo</h1>
           
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
@@ -119,54 +121,51 @@
   <br>
   <section class="showcase">
     <div class="container-fluid p-0">
-      <div class="row no-gutters">
 
+
+
+
+
+
+    <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url(<?= base_url('assets/images/eletronic_trash.png')?>);"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Equipamentos que não funcionam</h2>
+          <p class="lead mb-0">O que fazer com aquele equipamento eletrônico que ficou velho ou já não serve mais? Esse problema pode ser solucionado com o auxílio do Projeto de Recondicionamento e Reciclagem de Resíduos Eletroeletrônicos (E-Lixo), que funciona, desde 2013, na sala A-08 do Campus Natal - Zona Norte.
+
+              O que é o E-lixo?
+            O E-Lixo se constitui em um espaço de prática profissional aos estudantes dos cursos técnicos em Eletrônica, Informática e Manutenção e Suporte em Informática e promove, ainda, treinamentos, palestras e pesquisas relacionadas ao tema. A iniciativa oferece também assistência técnica a servidores e alunos do Campus no que diz respeito a equipamentos eletroeletrônicos.
+
+          </p>
+        </div>
+      </div>
+
+
+
+      <div class="row no-gutters">
         <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url(<?= base_url('assets/images/bg-showcase-1.jpg')?>);"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>O que é o E-lixo?</h2>
+        <h2>O que é o E-lixo?</h2>
           <p class="lead mb-0">O E-Lixo se constitui em um espaço de prática profissional aos estudantes dos cursos técnicos em Eletrônica, Informática e Manutenção e Suporte em Informática e promove, ainda, treinamentos, palestras e pesquisas relacionadas ao tema. A iniciativa oferece também assistência técnica a servidores e alunos do Campus no que diz respeito a equipamentos eletroeletrônicos.</p>
-        </div>
+        
+         </div>
       </div>
-      </div>
-      <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
-        <div class="col-lg-12 order-lg-1 my-auto showcase-text">
-          <h2>Notícias</h2>
-          <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  <!-- Testimonials -->
-  <section class="testimonials text-center bg-light">
-    <div class="container">
-      <h2 class="mb-5">What people are saying...</h2>
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-            <h5>Margaret E.</h5>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-            <h5>Fred S.</h5>
-            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-            <h5>Sarah W.</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
-          </div>
-        </div>
-      </div>
+
+      
+
     </div>
-  </section>
+    <div class="row no-gutters">
+        <div class="col-lg-12 order-lg-1 my-auto showcase-text">
+        <h2>Extensão</h2>
+          <p class="lead mb-0">Os representantes do projeto já têm levado a ideia a escolas e instituições interessadas em conhecer o trabalho realizado no Instituto. Na tarde da sexta-feira (19), o grupo visitou o Centro Estadual de Educação Profissional Senador Jessé Pinto Freire (Cenep), localizado no bairro Petrópolis. A visita contou com a participação do coordenador Ailton Torres e dos extensionistas Ayrton Rocha, Beatriz Nogueira e Vitoria Gomes.
+
+Na ocasião, no laboratório de informática da instituição de ensino, o grupo apresentou as atividades e as oportunidades do projeto a uma turma de 30 alunos dos cursos de Informática e Suporte em Informática.</p>
+
+         </div>
+      </div>
+  </section><br/>
+  
 
   <!-- Call to Action -->
   <section class="call-to-action text-white text-center">
@@ -174,16 +173,15 @@
     <div class="container">
       <div class="row">
         <div class="col-xl-9 mx-auto">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
+          <h2 class="mb-4">Doe equipamentos</h2>
+           <h5> <p>Para doar equipamentos ou obter mais informações, basta enviar um email com o título "e-lixo" para ailton.torres@ifrn.edu.br.</p> </h5>
+
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
           <form>
             <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
               <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
+                
               </div>
             </div>
           </form>
@@ -199,40 +197,12 @@
         <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
           <ul class="list-inline mb-2">
             <li class="list-inline-item">
-              <a href="#">About</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Contact</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Terms of Use</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Privacy Policy</a>
+              <a href="http://portal.ifrn.edu.br/campus/reitoria/noticias/e-lixo-projeto-do-campus-zona-norte-ganha-destaque-como-solucao-para-reaproveitamento-de-eletronicos">IFRN</a>
             </li>
           </ul>
-          <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2019. All Rights Reserved.</p>
-        </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-facebook fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw"></i>
-              </a>
-            </li>
+          <p>Rua Dr. Nilo Bezerra Ramalho, 1692, Tirol CEP: 59015-300 Natal - RN</p>        
+          <p>E-mail: comunicacao.reitoria@ifrn.edu.br</p>          
+          <p>Telefone: (84) 4005-0757</p>
           </ul>
         </div>
       </div>
---
+++ application/views/n-autenticado/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="theme-color" content="#4cd964">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+  <meta name="author" content="GeeksLabs">
+  <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+  <link rel="shortcut icon" href="<?php echo base_url('assets/img/gael.png')?>">
+
+  <title>GAEL - Login</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="<?php echo base_url('assets/vendor/bootstrap-4.1/bootstrap.min.css')?>" rel="stylesheet">
+  <!-- bootstrap theme -->
+  <link href="<?php echo base_url('assets/vendor/bootstrap-4.1/bootstrap-theme.css')?>" rel="stylesheet">
+  <!--external css-->
+  <!-- font icon -->
+  <link href="<?php echo base_url('assets/css/elegant-icons-style.css')?>" rel="stylesheet" />
+  <link href="<?php echo base_url('assets/css/font-awesome.css')?>" rel="stylesheet" />
+  <!-- Custom styles -->
+  <link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('assets/css/style-responsive.css')?>" rel="stylesheet" />
+
+ 
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-sm-4"> 
+      
+      </div>
+      <div class="col-sm-4" >
+        <?php if ($this->session->flashdata('success') == TRUE):?>
+            <div class="alert alert-danger">
+              <h2><?= $this->session->flashdata('success')?></h2>
+            </div>
+            
+
+        <?php endif;?>
+
+        <?php if ($this->session->flashdata('error')):?>
+          <div class="alert alert-danger" style="margin-bottom: -16px;">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+            <h5><?= $this->session->flashdata('error')?></h5>
+            
+          </div>
+          
+        <?php endif;?>
+        <script>
+        $('.alert').alert();
+        </script>
+      </div>
+      <div class="col-sm-4">
+      </div>
+    </div>
+
+    <form class="login-form form-login-container mb-5" method="post" action="<?php echo base_url('index.php/login/autenticar')?>">
+      <div id="" class="login-wrap" style="margin-top: -179px;">
+        <p class="login-img"><img src="<?php echo base_url('assets/img/gael.png')?>" width="80"></i></p>
+        <h4 class="text-center" style="color: black;">
+          <strong>Sistema Integrado para o Gerenciamento de Resíduos Eletroeletrônicos</strong>  
+        
+        </h4 >
+        <div class="input-group">
+          <span class="input-group-addon blr"><i class="icon_profile"></i></span>
+          <input type="text" class="form-control brr" name="u_email" placeholder="E-mail" autofocus>
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon blr"><i class="icon_key_alt"></i></span>
+          <input type="password" class="form-control brr" name="senha" placeholder="Senha">
+        </div>
+        <label style="color: black;" class="checkbox">
+                <input  type="checkbox" value="remember-me">
+                Lembrar
+                <span  class="pull-right"> <a href="" style="color: black;">Esqueceu a senha?</a></span>
+            </label>
+          <button type="submit" class="text-light btn btn-success btn-lg btn-block br-5" >Logar </button>
+
+      </div>
+    </form>
+    <div class="text-right">
+
+    </div>
+  </div>
+
+  <script src="<?= base_url('assets/vendor/jquery-3.2.1.min.js')?>"></script>
+    <!-- Bootstrap JS-->
+    <script src="<?= base_url('assets/vendor/bootstrap-4.1/popper.min.js')?>"></script>
+    <script src="<?= base_url('assets/vendor/bootstrap-4.1/bootstrap.min.js')?>"></script>
+</body>
+
+</html>
